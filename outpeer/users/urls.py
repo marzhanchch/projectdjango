ce782c27f38f46a7b4c74d301027ceb0a7273549
marzhanzhumagulova <marzhanchch@gmail.com>
2025-03-12 16:08:51 +0500

    Исправила ошибки в urls.py и views.py

--- outpeer/users/urls.py
+++ outpeer/users/urls.py
@@ -1,7 +1,9 @@
+
 from django.urls import path
-from .views import RegisterView, UserListView
+from .views import RegisterView, UserListView, VerifyCodeView  
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("users/", UserListView.as_view(), name="user-list"),
+    path("verify-code/", VerifyCodeView.as_view(), name="verify-code"),  
 ]
--- outpeer/users/views.py
+++ outpeer/users/views.py
@@ -7,7 +7,6 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAdminUser
 from django.contrib.auth import get_user_model
 from .models import User
 from .serializers import RegisterSerializer, UserSerializer
@@ -34,6 +33,27 @@ class RegisterView(APIView):
             return Response({"message": "Код отправлен на email"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+# Подтверждение кода
+class VerifyCodeView(APIView):
+    def post(self, request):
+        email = request.data.get("email")
+        code = request.data.get("code")
+
+        if not email or not code:
+            return Response({"error": "Email и код обязательны"}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            user = User.objects.get(email=email)
+            if user.verification_code == int(code):
+                user.is_active = True  # Активируем пользователя
+                user.verification_code = ""  # Очищаем код
+                user.save()
+                return Response({"message": "Аккаунт подтверждён"}, status=status.HTTP_200_OK)
+            else:
+                return Response({"error": "Неверный код"}, status=status.HTTP_400_BAD_REQUEST)
+        except User.DoesNotExist:
+            return Response({"error": "Пользователь не найден"}, status=status.HTTP_404_NOT_FOUND)
+
 # Пагинация и просмотр пользователей
 class CustomPagination(PageNumberPagination):
     page_size = 5
@@ -44,4 +64,3 @@ class UserListView(ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     pagination_class = CustomPagination
-    permission_classes = [IsAdminUser]
